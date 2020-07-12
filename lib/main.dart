33c4d0891d86cb11385c3d092c77eb4ac7146e46
dainavipurav <dainavipurav@gmail.com>
2020-07-12 20:47:15 +0530

    main.dart - Added routes for navigating screens
    home.dart
    - Added button to navigate choose location screen
    
    choose_location.dart
    - method override to initialize widgets
    - added trial counter variable
    - setstate functon added for change in data

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 import 'package:world_time_app/pages/home.dart';
-import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation()
+    },
   ));
 }
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,31 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    print('Init function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('Build Function Ran');
     return Scaffold(
-      body: Text('Chose Location Screen'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Coose Location'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: RaisedButton(
+          child: Text('Counter is $counter'),
+          onPressed: () {
+            setState(() {
+              counter = counter + 1;
+            });
+          }),
     );
   }
 }
