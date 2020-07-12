179c2a0d5aa92bb2e2a6beef2a43c451ce5fec83
dainavipurav <dainavipurav@gmail.com>
2020-07-12 22:53:44 +0530

    world_time.dart - create separate class for api request - constructor created - variables created for fetching current time
    loading.dart
    - call instance of world_time.dart class to fetch the current time of particular location
    - display time and date on screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time_app/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,25 +7,28 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-    Response response =
-        await get('https://jsonplaceholder.typicode.com/todos/1');
+  String time = 'Loading';
 
-    Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+  void setupWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: "Mumbai", flag: "india.png", url: 'Asia/Kolkata');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Loading Screen')),
+      body: Padding(padding: EdgeInsets.all(50.0), child: Text(time)),
     );
   }
 }
